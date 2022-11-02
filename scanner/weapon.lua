d7ef3e181c99ca990e751bab5c241fb099faa3df
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-11-02 08:32:51 +0100

    Add more weapon related scans
    New scans
    - weapon.powerCost
    - weapon.powerList - a list of power cores powering weapon
    - weapon.upgradeListA - a list of power cores for upgrade A
    - weapon.upgradeListB - a list of power cores for upgrade B

--- scanner/weapon.lua
+++ scanner/weapon.lua
@@ -53,10 +53,109 @@ scans.class = inheritClass(Scan, {
 	end
 })
 
+scans.powerCost = inheritClass(Scan, {
+	id = "PowerCost",
+	name = "Weapon Power Cost",
+	prerequisiteScans = weaponPrerequisites,
+	access = "RW",
+	dataType = "int",
+	action = function(self)
+		local cores = math.random(7,13)
+		prepareScanWeapon{ PowerCost = cores }
+		prepareScanPawn{ SkillList = {"memedit_scanWeapon"} }
+		local pawn = PAWN_FACTORY:CreatePawn("memedit_scanPawn")
+		local weaponIndex = 1
+
+		self:searchWeapon(pawn, weaponIndex, cores)
+		self:evaluateResults()
+	end
+})
+
+scans.powerList = inheritClass(Scan, {
+	id = "PowerList",
+	name = "Weapon Power List",
+	prerequisiteScans = weaponPrerequisites,
+	access = "R",
+	dataType = "IntList",
+	actions = {
+		function(self)
+			local weaponIndex = 1
+			local cores = math.random(7,13)
+			local powerList = {}
+			for i = 1, cores do
+				powerList[i] = 1
+			end
+
+			prepareScanWeapon{
+				PowerCost = cores,
+				Upgrades = 0,
+				UpgradeCost = {0,0}
+			}
+			prepareScanPawn{ SkillList = {"memedit_scanWeapon"} }
+			local pawn = PAWN_FACTORY:CreatePawn("memedit_scanPawn")
+
+			self:searchWeapon(pawn, weaponIndex, powerList, "int")
+			self:evaluateResults()
+		end
+	}
+})
+
 scans.type = inheritClass(scans.baseType, {
 	id = "Type",
 	name = "Weapon Type",
 	expectedResultIndex = 2,
+})
+
+scans.upgradeListA = inheritClass(Scan, {
+	id = "UpgradeListA",
+	name = "Weapon Upgrade List A",
+	prerequisiteScans = weaponPrerequisites,
+	access = "R",
+	dataType = "IntList",
+	action = function(self)
+		local weaponIndex = 1
+		local cores = math.random(7,13)
+		local powerList = {}
+		for i = 1, cores do
+			powerList[i] = 0
+		end
+		prepareScanWeapon{
+			PowerCost = 0,
+			Upgrades = 2,
+			UpgradeCost = {cores,0}
+		}
+		prepareScanPawn{ SkillList = {"memedit_scanWeapon"} }
+		local pawn = PAWN_FACTORY:CreatePawn("memedit_scanPawn")
+
+		self:searchWeapon(pawn, weaponIndex, powerList, "int")
+		self:evaluateResults()
+	end
+})
+
+scans.upgradeListB = inheritClass(Scan, {
+	id = "UpgradeListB",
+	name = "Weapon Upgrade List B",
+	prerequisiteScans = weaponPrerequisites,
+	access = "R",
+	dataType = "IntList",
+	action = function(self)
+		local weaponIndex = 1
+		local cores = math.random(7,13)
+		local powerList = {}
+		for i = 1, cores do
+			powerList[i] = 0
+		end
+		prepareScanWeapon{
+			PowerCost = 0,
+			Upgrades = 2,
+			UpgradeCost = {0,cores}
+		}
+		prepareScanPawn{ SkillList = {"memedit_scanWeapon"} }
+		local pawn = PAWN_FACTORY:CreatePawn("memedit_scanPawn")
+
+		self:searchWeapon(pawn, weaponIndex, powerList, "int")
+		self:evaluateResults()
+	end
 })
 
 return scans
