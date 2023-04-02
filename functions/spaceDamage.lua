63988ee3d87e43331eff0cdb6f627a7ad7d914d9
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2023-04-02 17:45:09 +0200

    Add SpaceDamage functions
    ### New functions:
    - GetAnimState
    - GetGrappleAnim
    - GetGrappleTarget
    - GetMoveType
    - SetAnimState
    - SetGrappleAnim
    - SetGrappleTarget
    - SetMoveType

--- functions/spaceDamage.lua
+++ functions/spaceDamage.lua
@@ -1,4 +1,72 @@
 
+function SpaceDamage:GetAnimState()
+	Assert.Equals("userdata", type(self), "Argument #0")
+
+	local result
+
+	try(function()
+		result = memedit:require().spaceDamage.getAnimState(self)
+	end)
+	:catch(function(err)
+		error("memedit.dll: "..tostring(err))
+	end)
+
+	return result
+end
+
+
+function SpaceDamage:GetGrappleAnim()
+	Assert.Equals("userdata", type(self), "Argument #0")
+
+	local result
+
+	try(function()
+		result = memedit:require().spaceDamage.getGrappleAnim(self)
+	end)
+	:catch(function(err)
+		error("memedit.dll: "..tostring(err))
+	end)
+
+	return result
+end
+
+
+function SpaceDamage:GetGrappleTarget()
+	Assert.Equals("userdata", type(self), "Argument #0")
+
+	local result
+
+	try(function()
+		local memedit = memedit:require()
+		result = Point(
+			memedit.spaceDamage.getGrappleTargetX(self),
+			memedit.spaceDamage.getGrappleTargetY(self)
+		)
+	end)
+	:catch(function(err)
+		error("memedit.dll: "..tostring(err))
+	end)
+
+	return result
+end
+
+
+function SpaceDamage:GetMoveType()
+	Assert.Equals("userdata", type(self), "Argument #0")
+
+	local result
+
+	try(function()
+		result = memedit:require().spaceDamage.getMoveType(self)
+	end)
+	:catch(function(err)
+		error("memedit.dll: "..tostring(err))
+	end)
+
+	return result
+end
+
+
 function SpaceDamage:GetProjectileArt()
 	Assert.Equals("userdata", type(self), "Argument #0")
 
@@ -48,6 +116,60 @@ function SpaceDamage:GetType()
 	end)
 
 	return result
+end
+
+
+function SpaceDamage:SetAnimState(busyState)
+	Assert.Equals("userdata", type(self), "Argument #0")
+	Assert.Equals("number", type(busyState), "Argument #1")
+
+	try(function()
+		memedit:require().spaceDamage.setAnimState(self, busyState)
+	end)
+	:catch(function(err)
+		error("memedit.dll: "..tostring(err))
+	end)
+end
+
+
+function SpaceDamage:SetGrappleAnim(anim)
+	Assert.Equals("userdata", type(self), "Argument #0")
+	Assert.Equals("string", type(anim), "Argument #1")
+
+	try(function()
+		memedit:require().spaceDamage.setGrappleAnim(self, anim)
+	end)
+	:catch(function(err)
+		error("memedit.dll: "..tostring(err))
+	end)
+end
+
+
+function SpaceDamage:SetGrappleTarget(target)
+	Assert.Equals("userdata", type(self), "Argument #0")
+	Assert.TypePoint(target, "Argument #1")
+
+	try(function()
+		local memedit = memedit:require()
+		memedit.spaceDamage.setGrappleTargetX(self, target.x)
+		memedit.spaceDamage.setGrappleTargetY(self, target.y)
+	end)
+	:catch(function(err)
+		error("memedit.dll: "..tostring(err))
+	end)
+end
+
+
+function SpaceDamage:SetMoveType(moveType)
+	Assert.Equals("userdata", type(self), "Argument #0")
+	Assert.Equals("number", type(moveType), "Argument #1")
+
+	try(function()
+		memedit:require().spaceDamage.setMoveType(self, moveType)
+	end)
+	:catch(function(err)
+		error("memedit.dll: "..tostring(err))
+	end)
 end
 
 
