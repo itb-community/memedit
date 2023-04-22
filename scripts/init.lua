4005f32d4d497160a6a42a774dd242f9d6508944
tos-x <61102907+tos-x@users.noreply.github.com>
2023-04-22 15:05:36 -0400

    Update version

--- scripts/init.lua
+++ scripts/init.lua
@@ -2,8 +2,8 @@
 local extension = {
 	id = "memedit",
 	name = "memedit",
-	version = "1.1.3",
-	modApiVersion = "2.9.0",
+	version = "1.1.4",
+	modApiVersion = "2.9.2",
 	gameVersion = "1.2.88",
 	icon = "img/icon.png",
 	isExtension = true,
